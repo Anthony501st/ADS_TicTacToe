65ff1b55fa981c20a4010ce8d10fa34406358a80
Anthony501st <85151725+Anthony501st@users.noreply.github.com>
2021-08-21 21:01:06 +1000

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef GAME_H
+#define GAME_H
+#endif
+void introduction();
+bool is_winner();
+bool filled_up();   //filled_up()
+void draw();
+void set_position();
+void update_board();
+void change_player();
+void take_turn();
+void end_game();
+
